06356b5c705354c4294ad35728dc5d2c60fa4107
BlackWtea <348701262@qq.com>
2019-08-25 22:46:32 +0800

    github login

---
+++ src/main/java/com/mashen/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package com.mashen.community.controller;
+
+import com.mashen.community.dto.AccesstokenDto;
+import com.mashen.community.dto.GithubUser;
+import com.mashen.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 陈俊龙
+ *
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${git.client.id}")
+    private String clientId;
+    @Value("${git.client.secret}")
+    private String clientSecret;
+    @Value("${git.redirect.uri}")
+    private String redirectUri;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state")String state){
+
+        AccesstokenDto accesstokenDto = new AccesstokenDto();
+        accesstokenDto.setClient_id(clientId);
+        accesstokenDto.setClient_secret(clientSecret);
+        accesstokenDto.setRedirect_uri(redirectUri);
+        accesstokenDto.setCode(code);
+        accesstokenDto.setState(state);
+        String accessToken = githubProvider.getAccesstoken(accesstokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName()+user.getBio());
+        return "index";
+    }
+}
+
